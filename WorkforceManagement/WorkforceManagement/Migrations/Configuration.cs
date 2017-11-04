d148d2794c91b814242d24a9fff9cb7d6085e86d
bryanhmiller <bryankalos@hotmail.com>
2017-11-04 12:18:55 -0500

    Added seed data for computers, employees

--- WorkforceManagement/WorkforceManagement/Migrations/Configuration.cs
+++ WorkforceManagement/WorkforceManagement/Migrations/Configuration.cs
@@ -20,13 +20,62 @@ namespace WorkforceManagement.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
-            //
-                context.Departments.AddOrUpdate(
+
+            Departments petProducts = new Departments { Name = "Pet products", Description = "Cat shoes", Type = DepartmentType.Shoes };
+            Departments gadgets = new Departments { Name = "Gadgets", Description = "Electronic key", Type = DepartmentType.Electronics };
+            Departments dishes = new Departments { Name = "Dishes", Description = "China dinnerware", Type = DepartmentType.Housewares };
+            context.Departments.AddOrUpdate(
                   p => p.Name,
-                  new Departments { Name = "Pet products", Description = "Cat shoes", Type = DepartmentType.Shoes },
-                  new Departments { Name = "Gadget", Description = "Electronic key", Type = DepartmentType.Electronics },
-                  new Departments { Name = "Dishes", Description = "China dinnerware", Type = DepartmentType.Housewares }
+                  petProducts,
+                  gadgets,
+                  dishes
                 );
+
+            context.Employees.AddOrUpdate(
+              p => p.JobTitle,
+              new Employees
+              {
+                  JobTitle = "Chief Simpsons Officer",
+                  FirstName = "Dwayne",
+                  LastName = "Pate",
+                  Departments = gadgets,
+                  StartDate = new DateTime(2017, 1, 1)
+              },
+              new Employees
+              {
+                  JobTitle = "Chief Chief",
+                  FirstName = "Anessa",
+                  LastName = "Ortner",
+                  Departments = petProducts,
+                  StartDate = new DateTime(2017, 1, 1)
+              },
+              new Employees
+              {
+                  JobTitle = "Chief Butcher",
+                  FirstName = "Marcus",
+                  LastName = "Gill",
+                  Departments = dishes,
+                  StartDate = new DateTime(2017, 1, 1)
+              },
+              new Employees
+              {
+                  JobTitle = "Chief Baker",
+                  FirstName = "Heather",
+                  LastName = "Thacker",
+                  Departments = petProducts,
+                  StartDate = new DateTime(2017, 1, 1)
+              },
+              new Employees
+              {
+                  JobTitle = "Chief Candlestick Maker",
+                  FirstName = "Bryan",
+                  LastName = "Miller",
+                  Departments = dishes,
+                  StartDate = new DateTime(2017, 1, 1)
+              }
+            );
+
+
             //
         }
     }
